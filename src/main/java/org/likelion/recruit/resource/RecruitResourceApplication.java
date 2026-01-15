cf955754cf6402228952d6d70ca4d0be7796741d
geoKim <kgeo6264@gmail.com>
2026-01-16 02:38:40 +0900

    init: Set up likelion-resource server

---
+++ src/main/java/org/likelion/recruit/resource/RecruitResourceApplication.java
@@ -0,0 +1,13 @@
+package org.likelion.recruit.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitResourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/likelion/recruit/resource/RecruitResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.likelion.recruit.resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
