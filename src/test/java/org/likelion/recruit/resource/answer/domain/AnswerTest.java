b5b9862e8b98fc3d1a82ad7db4ed2d1d8066cf46
geooeg <kgeo6264@gmail.com>
2026-01-17 22:42:37 +0900

    test: Implement Answer domain test

--- src/test/java/org/likelion/recruit/resource/answer/domain/AnswerTest.java
+++ src/test/java/org/likelion/recruit/resource/answer/domain/AnswerTest.java
@@ -1,7 +1,17 @@
 package org.likelion.recruit.resource.answer.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.likelion.recruit.resource.application.domain.Application;
+import org.likelion.recruit.resource.common.domain.Part;
+import org.likelion.recruit.resource.question.domain.Question;
+import org.likelion.recruit.resource.question.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AnswerTest {
@@ -9,6 +19,31 @@ class AnswerTest {
     @Test
     void createAnswer() {
 
-        Answer answer = Answer.create
+        // question 생성
+        Integer commonNumber = 1;
+        String commonContent = "공통 질문";
+        Question.Type commonType = Question.Type.COMMON;
+
+        Question question = Question.create(commonNumber, commonContent, commonType);
+
+        // application 생성
+        String name = "김지오";
+        String studentNumber = "20201223";
+        String phoneNumber = "01012345678";
+        String passwordHash = "hashed-password";
+        String major = "수학과";
+        String doubleMajor = "복수전공";
+        Integer semester = 7;
+        Application.AcademicStatus status = Application.AcademicStatus.ENROLLED;
+        Part part = Part.BACKEND;
+
+        Application application = Application.create(name, studentNumber, phoneNumber,
+                passwordHash, major, doubleMajor, semester, status, part);
+
+        Answer answer = Answer.create("답변", question, application);
+
+        Assertions.assertThat(answer).isNotNull();
+        Assertions.assertThat(answer.getQuestion().getQuestionNumber()).isEqualTo(commonNumber);
+        Assertions.assertThat(answer.getApplication().getPart()).isEqualTo(part);
     }
 }
