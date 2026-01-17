82f588495041d7051aba8fff748858e97f6be20c
geoKim <kgeo6264@gmail.com>
2026-01-17 16:59:45 +0900

    feat: Implement Project.java

---
+++ src/main/java/org/likelion/recruit/resource/project/domain/Project.java
@@ -0,0 +1,41 @@
+package org.likelion.recruit.resource.project.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.likelion.recruit.resource.common.domain.BaseTimeEntity;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Project extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer cohort;
+
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(name = "instagram_url", nullable = false)
+    private String instagramUrl;
+
+    private Project(Integer cohort, String imageUrl, String description, String instagramUrl) {
+        this.cohort = cohort;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.instagramUrl = instagramUrl;
+    }
+
+    public static Project create(Integer cohort, String imageUrl, String description, String instagramUrl) {
+        return new Project(cohort, imageUrl, description, instagramUrl);
+    }
+}
