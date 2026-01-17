95ec0a1d22d58e09867e47bd72783a26ce69c1f9
geooeg <kgeo6264@gmail.com>
2026-01-17 22:57:34 +0900

    feat: Implement interview time entity

---
+++ src/main/java/org/likelion/recruit/resource/interviewTime/domain/InterviewTime.java
@@ -0,0 +1,40 @@
+package org.likelion.recruit.resource.interviewTime.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.likelion.recruit.resource.common.domain.BaseTimeEntity;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class InterviewTime extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "interview_time_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private LocalTime startTime;
+
+    @Column(nullable = false)
+    private LocalTime endTime;
+
+    private InterviewTime(LocalDate date, LocalTime startTime) {
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = startTime.plusMinutes(20);
+    }
+
+    public static InterviewTime create(LocalDate date, LocalTime startTime) {
+        return new InterviewTime(date, startTime);
+    }
+}
