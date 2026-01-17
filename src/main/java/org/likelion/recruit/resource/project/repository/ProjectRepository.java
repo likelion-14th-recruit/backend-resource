63a0156f423de1eb675462a1940f4642cd73c388
Geo <kgeo6264@gmail.com>
2026-01-17 19:09:54 +0900

    [FEAT] #6 Project Domain 개발
    - Project 엔티티 생성
    - Project 엔티티 테스트 생성

---
+++ src/main/java/org/likelion/recruit/resource/project/repository/ProjectRepository.java
@@ -0,0 +1,7 @@
+package org.likelion.recruit.resource.project.repository;
+
+import org.likelion.recruit.resource.project.domain.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project,Long> {
+}
