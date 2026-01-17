e0db4cfea995f84faf4dcc9532765b35e54cdc3a
geooeg <kgeo6264@gmail.com>
2026-01-17 19:05:16 +0900

    test: Implement Project domain test

--- src/test/java/org/likelion/recruit/resource/project/domain/ProjectTest.java
+++ src/test/java/org/likelion/recruit/resource/project/domain/ProjectTest.java
@@ -9,29 +9,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-@Transactional
 class ProjectTest {
-
-    @Autowired
-    private ProjectRepository projectRepository;
 
     @Test
     void createProject() {
-        Project project = Project.create(14, "http://likelion14th/image-url",
-                "테스트 프로젝트", "http://likelion14th/instagram-url");
+        Integer cohort = 14;
+        String imageUrl = "http://likelion14th/image-url";
+        String description = "테스트 프로젝트";
+        String instagramUrl = "http://likelion14th/instagram-url";
 
-        Project save = projectRepository.save(project);
+        Project project = Project.create(cohort,imageUrl,description,instagramUrl);
 
-        assertThat(save).isNotNull();
-        assertThat(save.getId()).isEqualTo(project.getId());
-        assertThat(save.getCohort()).isEqualTo(project.getCohort());
-        assertThat(save.getDescription()).isEqualTo(project.getDescription());
-        assertThat(save.getImageUrl()).isEqualTo(project.getImageUrl());
-        assertThat(save.getInstagramUrl()).isEqualTo(project.getInstagramUrl());
-        assertThat(save.getCreatedAt()).isNotNull();
-        assertThat(save.getUpdatedAt()).isNotNull();
-        System.out.println("생성 시각 : " + save.getCreatedAt());
-        System.out.println("업데이트 시각 : " + save.getCreatedAt());
+        assertThat(project.getCohort()).isEqualTo(cohort);
+        assertThat(project.getImageUrl()).isEqualTo(imageUrl);
+        assertThat(project.getDescription()).isEqualTo(description);
+        assertThat(project.getInstagramUrl()).isEqualTo(instagramUrl);
     }
 }
