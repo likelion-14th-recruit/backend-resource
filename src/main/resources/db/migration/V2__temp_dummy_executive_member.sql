eb251458f0da8bf93f41496f4bf4afe6736b6336
geooeg <kgeo6264@gmail.com>
2026-01-18 23:08:29 +0900

    feat: Create member dummy data

---
+++ src/main/resources/db/migration/V2__temp_dummy_executive_member.sql
@@ -0,0 +1,18 @@
+INSERT INTO executive_member(name, cohort, image_url, part, position, created_at, updated_at)
+VALUES
+    ('김지오', 14, 'http://dummy1','BACKEND', 'PRESIDENT',NOW(), NOW()),
+    ('이예나', 14, 'http://dummy2','FRONTEND', 'VICE_PRESIDENT',NOW(), NOW()),
+    ('전해찬', 14, 'http://dummy3','BACKEND', 'VICE_PRESIDENT',NOW(), NOW()),
+    ('송명은', 14, 'http://dummy4','PRODUCT_DESIGN', 'PART_LEADER',NOW(), NOW()),
+    ('오서현', 14, 'http://dummy5','PRODUCT_DESIGN', 'MEMBER',NOW(), NOW()),
+    ('천사은', 14, 'http://dummy6','PRODUCT_DESIGN', 'MEMBER',NOW(), NOW()),
+    ('문금미', 14, 'http://dummy7','FRONTEND', 'PART_LEADER',NOW(), NOW()),
+    ('주현수', 14, 'http://dummy8','FRONTEND', 'MEMBER',NOW(), NOW()),
+    ('한서정', 14, 'http://dummy9','FRONTEND', 'MEMBER',NOW(), NOW()),
+    ('최성민', 14, 'http://dummy10','BACKEND', 'PART_LEADER',NOW(), NOW()),
+    ('유지오', 14, 'http://dummy11','BACKEND', 'MEMBER',NOW(), NOW()),
+    ('전수아', 14, 'http://dummy12','BACKEND', 'MEMBER',NOW(), NOW());
+
+
+
+
