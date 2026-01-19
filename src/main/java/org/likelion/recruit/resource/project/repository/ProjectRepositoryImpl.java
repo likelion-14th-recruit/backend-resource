0b366be4f8bdaa1937611a9c5bf9f9331297533c
geooeg <kgeo6264@gmail.com>
2026-01-19 21:35:25 +0900

    feat: Implement ProjectRepositoryImpl

--- src/main/java/org/likelion/recruit/resource/project/repository/ProjectRepositoryImpl.java
+++ src/main/java/org/likelion/recruit/resource/project/repository/ProjectRepositoryImpl.java
@@ -1,12 +1,28 @@
 package org.likelion.recruit.resource.project.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.likelion.recruit.resource.project.domain.QProject;
+import org.likelion.recruit.resource.project.dto.command.ProjectSearchCommand;
+import org.likelion.recruit.resource.project.dto.result.ProjectSearchResult;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+
+import static org.likelion.recruit.resource.project.domain.QProject.project;
 
 @RequiredArgsConstructor
 public class ProjectRepositoryImpl implements ProjectRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Slice<ProjectSearchResult> searchProjectsSlice(ProjectSearchCommand command, Pageable pageable) {
+        queryFactory.select(Projections.constructor(ProjectSearchResult.class,
+                        project.imageUrl,
+                        project.description,))
+                .from(project)
+    }
 
 
 }
