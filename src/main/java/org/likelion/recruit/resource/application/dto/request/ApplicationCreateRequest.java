55085114de2bface0e7035461028ed82d174b283
geooeg <kgeo6264@gmail.com>
2026-01-20 15:06:21 +0900

    feat: Add validation in application create request

--- src/main/java/org/likelion/recruit/resource/application/dto/request/ApplicationCreateRequest.java
+++ src/main/java/org/likelion/recruit/resource/application/dto/request/ApplicationCreateRequest.java
@@ -1,5 +1,7 @@
 package org.likelion.recruit.resource.application.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.likelion.recruit.resource.application.domain.Application;
@@ -12,13 +14,29 @@ public class ApplicationCreateRequest {
 
     @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
+
+    @NotBlank(message = "학번은 필수 입력 값입니다.")
     private String studentNumber;
+
+    @NotBlank(message = "전화번호는 필수 입력 값입니다.")
     private String phoneNumber;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String password;
+
+    @NotBlank(message = "전공은 필수 입력 값입니다.")
     private String major;
+
+
     private String doubleMajor;
+
+    @NotNull(message = "학기는 필수 입력 값입니다.")
     private Integer semester;
+
+    @NotNull(message = "학적 상태는 필수 입력 값입니다.")
     private AcademicStatus academicStatus;
+
+    @NotNull(message = "지원 파트는 필수 입력 값입니다.")
     private Part part;
 
 }
