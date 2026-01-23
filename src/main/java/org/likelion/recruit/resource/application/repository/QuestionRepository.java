c14d2052ec8199ef56a7d2c218c63d3c61839e76
최성민 <smsm7288@gmail.com>
2026-01-23 17:01:31 +0900

    perf: optimize submission validation by reducing DB queries and enhancing integrity

--- src/main/java/org/likelion/recruit/resource/application/repository/QuestionRepository.java
+++ src/main/java/org/likelion/recruit/resource/application/repository/QuestionRepository.java
@@ -4,6 +4,8 @@ import org.likelion.recruit.resource.application.domain.Question;
 import org.likelion.recruit.resource.application.repository.custom.QuestionRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Long>, QuestionRepositoryCustom {
-    long countByType(Question.Type type);
+    long countByTypeIn(List<Question.Type> types);
 }
