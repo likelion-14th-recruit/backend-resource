41b97cb2b8d38e08a2b92d9c0990e6228490f536
최성민 <smsm7288@gmail.com>
2026-01-23 16:05:51 +0900

    refactor: resolve N+1 issue in answer retrieval and simplify DTOs

--- src/main/java/org/likelion/recruit/resource/application/repository/AnswerRepository.java
+++ src/main/java/org/likelion/recruit/resource/application/repository/AnswerRepository.java
@@ -4,12 +4,20 @@ import org.likelion.recruit.resource.application.domain.Answer;
 import org.likelion.recruit.resource.application.domain.Application;
 import org.likelion.recruit.resource.application.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.List;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
     Optional<Answer> findByApplicationAndQuestion(Application application, Question question);
-    List<Answer> findAllByApplication(Application application);
+
+    @Query("select a from Answer a " +
+            "join fetch a.question " +
+            "where a.application = :application")
+
+    List<Answer> findAllByApplicationWithQuestion(@Param("application") Application application);
+
     long countByApplication(Application application);
 }
