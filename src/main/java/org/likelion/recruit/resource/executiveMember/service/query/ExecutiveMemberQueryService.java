3485fcddd801552ff9005b18c96936e2b273c680
Geo <kgeo6264@gmail.com>
2026-01-27 02:40:50 +0900

    [REFACTOR] #161 Caffeine 및 Cache를 이용하여 최적화

--- src/main/java/org/likelion/recruit/resource/executiveMember/service/query/ExecutiveMemberQueryService.java
+++ src/main/java/org/likelion/recruit/resource/executiveMember/service/query/ExecutiveMemberQueryService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.likelion.recruit.resource.executiveMember.dto.command.MemberSearchCommand;
 import org.likelion.recruit.resource.executiveMember.dto.result.MemberSearchResult;
 import org.likelion.recruit.resource.executiveMember.repository.ExecutiveMemberRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +17,10 @@ public class ExecutiveMemberQueryService {
 
     private final ExecutiveMemberRepository executiveMemberRepository;
 
+    @Cacheable(
+            value = "executiveMembers",
+            key = "#command.part != null ? #command.part.name() : 'ALL'"
+    )
     public List<MemberSearchResult> searchMembers(MemberSearchCommand command){
         return executiveMemberRepository.findByCondition(command);
     }
