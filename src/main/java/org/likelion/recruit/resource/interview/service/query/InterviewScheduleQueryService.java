5292d5344fef0cd1d838f5a17f4a73c4021907a0
Yujioh <jioyu14@gmail.com>
2026-01-29 00:48:10 +0900

    feat: add InterviewScheduleQueryService

--- src/main/java/org/likelion/recruit/resource/interview/service/query/InterviewScheduleQueryService.java
+++ src/main/java/org/likelion/recruit/resource/interview/service/query/InterviewScheduleQueryService.java
@@ -2,6 +2,9 @@ package org.likelion.recruit.resource.interview.service.query;
 
 import org.likelion.recruit.resource.application.domain.Application;
 import org.likelion.recruit.resource.application.repository.ApplicationRepository;
+import org.likelion.recruit.resource.common.exception.BusinessException;
+import org.likelion.recruit.resource.common.exception.ErrorCode;
+import org.likelion.recruit.resource.interview.domain.InterviewSchedule;
 import org.likelion.recruit.resource.interview.dto.result.InterviewScheduleResult;
 import org.likelion.recruit.resource.interview.repository.InterviewScheduleRepository;
 import org.springframework.stereotype.Service;
@@ -16,7 +19,13 @@ public class InterviewScheduleQueryService {
 
     public InterviewScheduleResult getInterviewSchedule(String applicationPublicId) {
 
-        Application application=applicationRepository.findIdByPublicId(applicationPublicId)
+        Application application = applicationRepository.findByPublicId(applicationPublicId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.APPLICATION_NOT_EXISTS));
+
+        InterviewSchedule interviewSchedule = interviewScheduleRepository.findByApplication(application)
+                .orElseThrow(()-> new BusinessException(ErrorCode.INTERVIEW_NOT_SCHEDULED));
+
+        return InterviewScheduleResult.from(interviewSchedule);
 
     }
 }
