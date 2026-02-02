cce1551c4c68dd25fb67b8907f5c59d5dffd06f1
Geo Kim <kgeo6264@gmail.com>
2026-02-02 15:31:33 +0900

    [FEAT] #181 프로젝트 실제 데이터 삽입
    SonarCloud 오류는 데이터 삽입을 중복 데이터로 간주하여 그렇습니다.

---
+++ src/main/resources/db/migration/V15__dummy_project.sql
@@ -0,0 +1,428 @@
+INSERT INTO project(cohort, name, image_url, description, instagram_url, created_at, updated_at)
+VALUES
+    (13,
+     '화리보',
+     'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/30.png',
+     '지하철 이용객의 쾌적한 화장실 이용을 위한 리뷰·정보 공유 서비스',
+     'https://www.instagram.com/p/DS9BapNEklW/',
+     NOW(),NOW()
+    ),
+    (
+        13,
+        '티킷타',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/31.png',
+        '좌석·결제·QR 입장까지 한 번에! 학생 공연을 위한 통합 관리 및 예매 서비스',
+        'https://www.instagram.com/p/DS9BR6oEsVX/',
+        NOW(), NOW()
+    ),
+    (
+        13,
+        'TripShot',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/32.png',
+        '여행의 추억을 더욱 생생하게 간직하기 위한 사진 중심 기록 서비스',
+        'https://www.instagram.com/p/DS9A2q_Eo4r/',
+        NOW(), NOW()
+    ),
+    (
+        13,
+        'RunCord',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/33.png',
+        '기존 러닝 기록에 아카이빙을 더해 러닝의 의미와 재미를 확장하는 서비스',
+        'https://www.instagram.com/p/DS9Ah0WkvHB/',
+        NOW(), NOW()
+    ),
+    (
+        13,
+        '쭈불',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/34.png',
+        '선상 낚시의 예약 및 운영을 위한 서비스',
+        'https://www.instagram.com/p/DS9ASBrkli7/',
+        NOW(), NOW()
+    ),
+    (
+        13,
+        '꼬꼬리스트',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/35.png',
+        '신촌·홍대 로컬 가게와 팝업스토어를 한눈에! AI 번역·추천으로 나만의 로컬 경험을 즐길 수 있는 서비스',
+        'https://www.instagram.com/p/DPvKaJMES1b/',
+        NOW(), NOW()
+    ),
+    (
+        13,
+        '마실',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/36.png',
+        '시니어의 만남이 지역의 활력이 되는 모임 추천 서비스',
+        'https://www.instagram.com/p/DPvKJhWkZ5S/',
+        NOW(), NOW()
+    ),
+    (
+        13,
+        '오늘도 축제로다',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/37.png',
+        '지역 축제의 기획자, 업체, 단기 근로자를 하나로 연결하는 맞춤형 서비스',
+        'https://www.instagram.com/p/DPvJwUCEd7L/',
+        NOW(), NOW()
+    ),
+    (
+        13,
+        '온마을',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/38.png',
+        '디지털기기 도움으로 어르신과 청년이 하나가 되는 서비스',
+        'https://www.instagram.com/p/DPvJJwxkTD-/',
+        NOW(), NOW()
+    ),
+    (
+        13,
+        '일드림',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/39.png',
+        '시니어 친화적 고용 매칭 서비스',
+        'https://www.instagram.com/p/DPvI7anEeyn/',
+        NOW(), NOW()
+    ),
+    (
+        13,
+        'Zarit',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/40.png',
+        '학생에겐 실시간 맞춤 추천으로 헛걸음을 줄이고, 지역 카페엔 신규 고객을 연결하는 AI 추천 서비스',
+        'https://www.instagram.com/p/DPvIp6CkY5L/',
+        NOW(), NOW()
+    ),
+    (
+        13,
+        'CARDINAL',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/41.png',
+        '총동연 하랑과 멋사 서강 13기 협업으로 만든 2025 서강대 가을문화제 CARDINAL 웹페이지',
+        'https://www.instagram.com/p/DO8KzxlEmcb/',
+        NOW(), NOW()
+    ),
+    (
+        13,
+        '13기 공식 홈페이지',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/42.png',
+        '멋쟁이사자처럼 서강대학교 13기 공식 웹페이지',
+        'https://www.instagram.com/p/DGNnFpUSK9p/',
+        NOW(), NOW()
+    ),
+    (
+        12,
+        '동문서잡',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/43.png',
+        '급한 알바 대타부터 단기 일자리까지, 동문 경력 기반 안심 매칭 서비스',
+        'https://www.instagram.com/p/DEmDO38SFvg/',
+        NOW(), NOW()
+    ),
+    (
+        12,
+        'NEWREALS',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/44.png',
+        '누구나 쉽고 재미있게 참여하는 개인 맞춤 뉴스 서비스',
+        'https://www.instagram.com/p/DElzG4MS5ru/',
+        NOW(), NOW()
+    ),
+    (
+        12,
+        '퍼니페니',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/45.png',
+        '초등학생의 쉽고 재밌는 경제 교육을 위한 롤플레잉 지원 서비스',
+        'https://www.instagram.com/p/DEly0cOS7gB/',
+        NOW(), NOW()
+    ),
+    (
+        12,
+        'INNERJOIN',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/46.png',
+        '모집부터 합불 통보까지 한 번에, 스마트한 동아리 리크루팅 서비스',
+        'https://www.instagram.com/p/DElxTk3SRdX/',
+        NOW(), NOW()
+    ),
+    (
+        12,
+        'UNICON',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/47.png',
+        '서강대 외국인·한국 학생이 쉽게 소통하고 정보를 나누는 교류 서비스',
+        'https://www.instagram.com/p/DElxDDZyHkL/',
+        NOW(), NOW()
+    ),
+    (
+        12,
+        '추억의 숲',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/48.png',
+        '가족이 함께 키운 나무에 우리 가족만의 추억을 열매로 남기는 기록 서비스',
+        'https://www.instagram.com/p/C--MY51SNzz/',
+        NOW(), NOW()
+    ),
+    (
+        12,
+        'Pebble',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/49.png',
+        '현대인들의 명언을 추천받고 자신만의 명언을 기록하며 잠시 쉬어갈 수 있는 힐링 서비스',
+        'https://www.instagram.com/p/C--LFRGyXbT/',
+        NOW(), NOW()
+    ),
+    (
+        12,
+        'BODA',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/50.png',
+        '어제를 돌아보고 오늘을 마주하는 AI 인터뷰형 에세이 서비스',
+        'https://www.instagram.com/p/C--KdvJyKps/',
+        NOW(), NOW()
+    ),
+    (
+        12,
+        'PET:EVER',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/51.png',
+        '반려동물과의 시간을 기록해 소중한 추억으로 남기는 서비스',
+        'https://www.instagram.com/p/C--J-zgSR8I/',
+        NOW(), NOW()
+    ),
+    (
+        11,
+        '연애 고수 서담이',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/52.png',
+        '멋사와 인사이트가 함께 만든, 연애 고수 서담이와 대화하는 AI 연애 상담 챗봇 서비스',
+        'https://www.instagram.com/p/C4DJaIKyUX8/',
+        NOW(), NOW()
+    ),
+    (
+        12,
+        '12기 공식 홈페이지',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/53.png',
+        '멋쟁이사자처럼 서강대학교 12기 공식 웹페이지',
+        'https://www.instagram.com/p/C3blnZqPngg/',
+        NOW(), NOW()
+    ),
+    (
+        11,
+        'sellpoint',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/54.png',
+        '투자자들에게 종목에 대한 전문가의 의견을 제시해 주는 AI 기반의 애널리틱스 서비스',
+        'https://www.instagram.com/p/C0MMNQTPMmO/',
+        NOW(), NOW()
+    ),
+    (
+        11,
+        'TU:PL',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/55.png',
+        '일정·진도·소통을 한 번에 관리하는 통합 과외 관리 서비스',
+        'https://www.instagram.com/p/C0MMHlVPkC6/',
+        NOW(), NOW()
+    ),
+    (
+        11,
+        'Crews',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/56.png',
+        '홍보부터 모집·면접자 관리까지 한 번에 하는 동아리 리쿠르팅 서비스',
+        'https://www.instagram.com/p/C0MMCCpPecO/',
+        NOW(), NOW()
+    ),
+    (
+        11,
+        'RTS',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/57.png',
+        '원하는 모든 과목을 로드맵에 담아 한눈에 관리하는 학점·수강 계획 서비스',
+        'https://www.instagram.com/p/C0ML68cPJfT/',
+        NOW(), NOW()
+    ),
+    (
+        11,
+        'Cardinal',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/58.png',
+        '축제 일정·지도·라인업까지 모두 담은 2023 서강대 축제 CARDINAL 웹페이지',
+        'https://www.instagram.com/p/CxXXS88P2Q9/',
+        NOW(), NOW()
+    ),
+    (
+        11,
+        'Sogang Type',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/59.png',
+        '테스트로 알아보는 당신의 서강생활 유형 SOGANG type 서비스',
+        'https://www.instagram.com/p/CpSmZrAv0WB/',
+        NOW(), NOW()
+    ),
+    (
+        10,
+        '번개콩',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/60.png',
+        '할인도 받고 밥도 함께 먹는 오프라인 번개 매칭 서비스',
+        'https://www.instagram.com/p/Cli8rCev7Xj/',
+        NOW(), NOW()
+    ),
+    (
+        10,
+        '네모',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/61.png',
+        '네모난 사진 속 모두의 추억을 담는 네컷 사진 아카이빙 및 공유 서비스',
+        'https://www.instagram.com/p/Cli8fQ5PwSY/',
+        NOW(), NOW()
+    ),
+    (
+        10,
+        'devStory',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/62.png',
+        '개발자들 간의 고민을 나누고 해결하는 개발자 커뮤니티 서비스',
+        'https://www.instagram.com/p/Cli8EpCvsWh/',
+        NOW(), NOW()
+    ),
+    (
+        10,
+        '점메추',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/63.png',
+        '오직 서강대생만을 위한 할인 맛집 추천 서비스',
+        'https://www.instagram.com/p/Cli7WsfPVfF/',
+        NOW(), NOW()
+    ),
+    (
+        10,
+        '지금 우리 서강은',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/64.png',
+        '이벤트·푸드트럭·플리마켓 정보를 담은 2022 서강문화제 웹페이지',
+        'https://www.instagram.com/p/Ci1lpbaPxx_/',
+        NOW(), NOW()
+    ),
+    (
+        9,
+        '동네에서 만나요',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/65.png',
+        '동네 주민과 함께하는 저렴하고 현명한 공동구매 서비스',
+        'https://www.instagram.com/p/CW20xhKPT0n/',
+        NOW(), NOW()
+    ),
+    (
+        9,
+        'ALLCAKES',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/66.png',
+        '주문 제작 케이크 가게를 검색·비교·결제하는 올인원 서비스',
+        'https://www.instagram.com/p/CW20rl_PQB5/',
+        NOW(), NOW()
+    ),
+    (
+        9,
+        'ZIP',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/67.png',
+        '나만의 인테리어를 만들고 공유하는 큐레이션 커뮤니티 서비스',
+        'https://www.instagram.com/p/CW20f_VvupV/',
+        NOW(), NOW()
+    ),
+    (
+        9,
+        '이음',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/68.png',
+        '생생하게 기억하고 싶은 이야기를 음악으로 잇는 서비스',
+        'https://www.instagram.com/p/CW20Y42PPV4/',
+        NOW(), NOW()
+    ),
+    (
+        9,
+        '누구나 세금',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/69.png',
+        '세무사 없이 쉽게 세금 관리하는 간편 세금 관리 서비스',
+        'https://www.instagram.com/p/CW20SF3v2yi/',
+        NOW(), NOW()
+    ),
+    (
+        8,
+        '옷짱',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/70.png',
+        '내 옷장을 관리하고 코디를 추천·교환까지 가능한 패션 매칭 서비스',
+        'https://www.instagram.com/p/CL_KeqdDaRx/',
+        NOW(), NOW()
+    ),
+    (
+        8,
+        '책화점',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/71.png',
+        '동네에서 쉽고 편하게 책을 찾고 거래하는 서비스',
+        'https://www.instagram.com/p/CL_KJJDDOqq/',
+        NOW(), NOW()
+    ),
+    (
+        8,
+        'SeeYa',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/72.png',
+        '연극 뮤지컬 시야 후기 공유 서비스',
+        'https://www.instagram.com/p/CL5_yO4j85E/',
+        NOW(), NOW()
+    ),
+    (
+        8,
+        'SPACE 16',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/73.png',
+        '서로 다른 16가지 유형의 사람들을 이해하는 MBTI 서비스',
+        'https://www.instagram.com/p/CL5_cUaDK4N/',
+        NOW(), NOW()
+    ),
+    (
+        8,
+        '서강고등학교',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/74.png',
+        '2019 서강문화제: 서강고등학교 웹사이트',
+        'https://www.instagram.com/p/B21xb50gxSv/',
+        NOW(), NOW()
+    ),
+    (
+        7,
+        'QRating',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/75.png',
+        '전공 기반 보상형 QnA 서비스',
+        'https://www.instagram.com/p/B0SIS5IDjZG/',
+        NOW(), NOW()
+    ),
+    (
+        7,
+        '가자미',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/76.png',
+        '다양한 공연의 홍보와 예매까지 할 수 있는 학생 공연 서비스',
+        'https://www.instagram.com/p/B0K95hgDi3a/',
+        NOW(), NOW()
+    ),
+    (
+        7,
+        '공감의 밤하늘',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/77.png',
+        '자신의 감정을 밤하늘에 기록하고, 서로 공감할 수 있는 감정 기록 서비스',
+        'https://www.instagram.com/p/B0IdBHTD31T/',
+        NOW(), NOW()
+    ),
+    (
+        7,
+        '사삼오오',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/78.png',
+        '4.3 사건을 기억할 수 있는 서비스',
+        'https://www.instagram.com/p/B0GfmwkDCSF/',
+        NOW(), NOW()
+    ),
+    (
+        7,
+        '2019 서강대학교 대동제',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/79.png',
+        '실시간 공연 정보, 푸드트럭과 마당사업 위치 등을 한눈에 볼 수 있는 2019 서강대학교 대동제 웹페이지',
+        'https://www.instagram.com/p/Bxeq7ZojD1q/',
+        NOW(), NOW()
+    ),
+    (
+        6,
+        '인아웃',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/80.png',
+        '회원가입 사이트 관리, 이용약관 확인 도우미 서비스',
+        'https://www.instagram.com/p/BvGBVA-gNrf/',
+        NOW(), NOW()
+    ),
+    (
+        6,
+        'POUCHMATE',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/81.png',
+        '내 파우치 속 인생템을 관리 & 공유하는 웹 화장대 서비스',
+        'https://www.instagram.com/p/BuFNEegFHEp/',
+        NOW(), NOW()
+    ),
+    (
+        6,
+        'BBOBADREAM',
+        'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/project/82.png',
+        '집에서 편하게 주문하고 본인 프린트물을 바로 가져갈 수 있는 테이크아웃 서비스',
+        'https://www.instagram.com/p/BtzNW3dljFo/',
+        NOW(), NOW()
+    );
+
+
+
