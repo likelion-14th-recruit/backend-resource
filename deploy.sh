7812c3c9168e954a774ef0ef9d34588a2a424cfb
Sua Jun <mjun3712@gmail.com>
2026-02-04 20:41:42 +0900

    fix: ip auto

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-EXIST_BLUE=$(docker ps | grep spring-blue)
+IS_BLUE=$(docker ps | grep spring-blue)
 
-if [ -z "$EXIST_BLUE" ]; then
-    echo "### BLUE 배포 시작 ###"
-    docker compose -f docker-compose.blue.yml up -d --build
-
-    sleep 30
-
-    echo "set \$service_url http://spring-blue:8080;" > ./nginx/service-url.inc
-
-    docker compose -f docker-compose.blue.yml exec -T nginx nginx -s reload
-
-    docker compose -f docker-compose.green.yml down
-    echo "### GREEN 종료 및 BLUE 전환 완료 ###"
-
+if [ -z "$IS_BLUE" ]; then
+  TARGET_COLOR="blue"
+  BEFORE_COLOR="green"
 else
-    echo "### GREEN 배포 시작 ###"
-    docker compose -f docker-compose.green.yml up -d --build
-
-    sleep 30
-
-    echo "set \$service_url http://spring-green:8080;" > ./nginx/service-url.inc
-
-    docker compose -f docker-compose.green.yml exec -T nginx nginx -s reload
-
-    docker compose -f docker-compose.blue.yml down
-    echo "### BLUE 종료 및 GREEN 전환 완료 ###"
+  TARGET_COLOR="green"
+  BEFORE_COLOR="blue"
 fi
+
+echo "### $TARGET_COLOR 배포 시작 ###"
+
+docker compose -f docker-compose.$TARGET_COLOR.yml up -d --build
+
+sleep 30
+
+NEW_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' spring-$TARGET_COLOR)
+
+if [ -z "$NEW_IP" ]; then
+  echo "에러: 새로운 컨테이너의 IP를 찾을 수 없습니다."
+  exit 1
+fi
+
+echo "set \$service_url http://$NEW_IP:8080;" > ./nginx/service-url.inc
+
+docker cp ./nginx/service-url.inc nginx:/etc/nginx/conf.d/service-url.inc
+docker exec nginx nginx -s reload
+
+echo "### $BEFORE_COLOR 종료 및 $TARGET_COLOR 전환 완료 ###"
+
+docker compose -f docker-compose.$BEFORE_COLOR.yml down
