bc648fe8216be47d5a5350ff0765dab4f9aa73fa
Sua Jun <mjun3712@gmail.com>
2026-02-04 20:52:17 +0900

    fix: deploy 찐막

--- deploy.sh
+++ deploy.sh
@@ -10,32 +10,23 @@ else
   BEFORE_COLOR="blue"
 fi
 
-echo "### $TARGET_COLOR 배포를 시작합니다 ###"
+echo "### $TARGET_COLOR 배포 시작 ###"
 
-docker compose -f docker-compose.$TARGET_COLOR.yml up -d --build
+docker compose -f docker-compose.$TARGET_COLOR.yml up -d spring-$TARGET_COLOR
 
-echo "스프링 부트($TARGET_COLOR)가 완전히 켜질 때까지 30초 대기합니다..."
+echo "스프링 부트($TARGET_COLOR)가 켜질 때까지 30초 대기..."
 sleep 30
 
 NEW_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' spring-$TARGET_COLOR)
 
-if [ -z "$NEW_IP" ]; then
-  echo "에러: $TARGET_COLOR 컨테이너의 IP를 찾을 수 없습니다."
-  exit 1
-fi
-
-echo "새로운 서버의 IP는 $NEW_IP 입니다. Nginx 지도를 업데이트합니다."
-
 echo "set \$service_url http://$NEW_IP:8080;" > ./nginx/service-url.inc
-
-docker start nginx
 docker cp ./nginx/service-url.inc nginx:/etc/nginx/conf.d/service-url.inc
 docker exec nginx nginx -s reload
 
 echo "### Nginx 연결 전환 완료! ###"
 
-echo "이전 서버($BEFORE_COLOR)를 안전하게 제거합니다."
+echo "이전 서버($BEFORE_COLOR)만 정밀 타격하여 제거합니다."
 docker stop spring-$BEFORE_COLOR 2>/dev/null
 docker rm spring-$BEFORE_COLOR 2>/dev/null
 
-echo "### $TARGET_COLOR 배포 및 무중단 전환 대성공! ###"
+echo "### 배포 완료! 이제 nginx랑 신규 서버만 남았습니다. ###"
