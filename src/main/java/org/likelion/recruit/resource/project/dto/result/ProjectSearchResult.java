d160f2ed864c60ce44f7decd8f79dc43ac5182b9
geooeg <kgeo6264@gmail.com>
2026-02-07 01:51:53 +0900

    fix: Revert commit

--- src/main/java/org/likelion/recruit/resource/project/dto/result/ProjectSearchResult.java
+++ src/main/java/org/likelion/recruit/resource/project/dto/result/ProjectSearchResult.java
@@ -6,12 +6,14 @@ import lombok.Getter;
 public class ProjectSearchResult {
     private String imageUrl;
     private String name;
+    private Integer cohort;
     private String description;
     private String instagramUrl;
 
-    public ProjectSearchResult(String imageUrl, String name, String description, String instagramUrl) {
+    public ProjectSearchResult(String imageUrl, String name, Integer cohort, String description, String instagramUrl) {
         this.imageUrl = imageUrl;
         this.name = name;
+        this.cohort = cohort;
         this.description = description;
         this.instagramUrl = instagramUrl;
     }
