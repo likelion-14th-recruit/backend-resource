acfcb1ae38d1b3af0e2eff02ed9b2755a488a3e6
Geo Kim <kgeo6264@gmail.com>
2026-02-07 23:02:41 +0900

    [FEAT] #237 CorsConfig 생성

---
+++ src/main/java/org/likelion/recruit/resource/common/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package org.likelion.recruit.resource.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        //로컬 개발 환경
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://localhost:5173");
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
