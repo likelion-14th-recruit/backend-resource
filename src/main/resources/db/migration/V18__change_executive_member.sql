fb1250313a5a0763979e28fd02d4a132ead34932
geooeg <kgeo6264@gmail.com>
2026-03-14 13:51:38 +0900

    fix: Add executive member in db

--- src/main/resources/db/migration/V18__change_executive_member.sql
+++ src/main/resources/db/migration/V18__change_executive_member.sql
@@ -9,4 +9,4 @@ INSERT INTO executive_member(name, cohort, image_url, part, position, created_at
 VALUES
 ('진현민', 14, 'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/executive-member/13.png','PRODUCT_DESIGN', 'MEMBER',NOW(), NOW()),
 ('문예담', 14, 'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/executive-member/14.png','PRODUCT_DESIGN', 'MEMBER',NOW(), NOW()),
-('이현서', 14, 'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/executive-member/15.png','PRODUCT_DESIGN', 'PART_LEADER',NOW(), NOW()),
+('이현서', 14, 'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/executive-member/15.png','PRODUCT_DESIGN', 'PART_LEADER',NOW(), NOW());
