2d7cea77f59e3b11ff5532fe77a54458aad2d7d1
geooeg <kgeo6264@gmail.com>
2026-03-14 13:18:37 +0900

    feat: Change executive member

---
+++ src/main/resources/db/migration/V18__change_executive_member.sql
@@ -0,0 +1,12 @@
+DELETE FROM executive_member
+WHERE name IN ('송명은', '천사은');
+
+UPDATE executive_member
+SET part = 'FRONTEND'
+WHERE name = '오서현';
+
+INSERT INTO executive_member(name, cohort, image_url, part, position, created_at, updated_at)
+VALUES
+('진현민', 14, 'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/executive-member/13.png','PRODUCT_DESIGN', 'MEMBER',NOW(), NOW()),
+('문예담', 14, 'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/executive-member/14.png','PRODUCT_DESIGN', 'MEMBER',NOW(), NOW()),
+('이현서', 14, 'https://likelion14th-recruit.s3.ap-northeast-2.amazonaws.com/executive-member/15.png','PRODUCT_DESIGN', 'PART_LEADER',NOW(), NOW()),
